b4099eaa886bd5562d43cab3011f1281543f1126
Kima32 <crki73@gmail.com>
2022-08-24 23:52:00 +0200

    1

---
+++ BUSSTATION/src/main/java/com/fis/BUSSTATION/BusstationApplication.java
@@ -0,0 +1,13 @@
+package com.fis.BUSSTATION;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusstationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusstationApplication.class, args);
+	}
+
+}
---
+++ BUSSTATION/src/test/java/com/fis/BUSSTATION/BusstationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fis.BUSSTATION;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusstationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
